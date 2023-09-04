1cb4cc5e0aa863978b85d50917203a0362513aa6
Hector Morgado <e3.corest@outlook.com>
2023-09-03 19:08:02 -0500

    Se agrega el la pagina electricidad la ley de ohm y ley de joule, de la misma manera su formulario, y se crea una funcion en calculation para trabajar con 3 argumentos

--- e3core&floriambra/scripts/calculations.js
+++ e3core&floriambra/scripts/calculations.js
@@ -25,7 +25,12 @@ const split_two = (number_one,number_two)=>{
     return number_one / number_two ;
 }
 
-function two_stage(form,box_one,box_two,calculation){
+const multiply_three_elev = (number_one,number_two,number_three)=>{
+    const value_const = 0.24
+    return value_const * (number_one**2) * number_two * number_three ;
+}
+
+function two_stage(form,box_one,box_two,calculation,extension){
 
     const form_main = document.getElementById(form);
 
@@ -48,13 +53,45 @@ function two_stage(form,box_one,box_two,calculation){
            totalFixed = totalFixed_total.toFixed(0);
         }
         form_main.appendChild(result)
-        result.innerHTML = `${totalFixed}`
+        result.innerHTML = `${totalFixed} ${extension}`
     
         console.log(totalFixed);
     })
     
 }
 
-two_stage('form_voltage','current1','endurance1',multiply_two);
-two_stage('form_current','voltage2','endurance2',split_two);
-two_stage('form_endurance','voltage3','current3',split_two);
+function three_stage(form,box_one,box_two,box_three,calculation,extension){
+    const form_main = document.getElementById(form);
+
+    form_main.addEventListener("submit",(event)=>{
+        event.preventDefault();
+        const variable_one = document.getElementById(box_one);
+        const variable_two = document.getElementById(box_two);
+        const variable_three = document.getElementById(box_three);
+        const number_one = parseFloat(variable_one.value);
+        const number_two = parseFloat(variable_two.value); 
+        const number_three = parseFloat(variable_three.value); 
+        const existingResults = form_main.querySelectorAll('p');
+        existingResults.forEach((result) => {
+            result.remove();
+        });
+        const result = document.createElement('p');
+        result.classList.add("result_form");
+        const total =  calculation(number_one,number_two,number_three);
+        let  totalFixed = total.toFixed(2);
+        totalFixed_total = parseFloat(totalFixed);
+        if (totalFixed_total % 1 == 0) {
+           totalFixed = totalFixed_total.toFixed(0);
+        }
+        form_main.appendChild(result)
+        result.innerHTML = `${totalFixed} ${extension}`
+    
+        console.log(totalFixed);
+    })
+}
+
+
+three_stage('form_joule','current_joule','endurance_joule','time_joule',multiply_three_elev,'Q')
+two_stage('form_voltage','current1','endurance1',multiply_two,'V');
+two_stage('form_current','voltage2','endurance2',split_two,'A');
+two_stage('form_endurance','voltage3','current3',split_two,'Ω');
